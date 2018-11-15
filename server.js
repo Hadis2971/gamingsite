839f2ed9b5d72570dc90eb5ecde5afb2fdca25c2
Albert Hofmann <albhof@email.com>
2018-11-15 13:04:14 +0100

    add templating engine exp-handlebars add public folder for static assets

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 const express = require("express");
+const exphbs  = require("express-handlebars");
+const path    = require("path");
 
 const app = express();
 app.set("port", (process.env.PORT || 3000));
 
+app.set("view engine", "handlebars");
+app.set("views", path.join(__dirname, "views"));
+app.engine("handlebars", exphbs({defaultLayout: "layout"}));
 
-
+app.use(express.static("public"));
 
 
 app.listen(app.get("port"), (err) => {
